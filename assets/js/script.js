0fd1dac6578577ffcd5736669ad24a54f4d53b84
Guilherme Pimentel <gpvieira18@gmail.com>
2023-01-09 17:18:22 -0300

    configurada a seleção dos botoes dos alimentos

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,23 @@ function selecionarComida(botaoClicado) {
     
         botaoClicado.classList.add('comidaselecionado')
 }
+
+function selecionarBebida(botaoClicado2) {
+    const botaoSelecionadoAntes2 = document.querySelector('.bebidaselecionado')
+
+    if (botaoSelecionadoAntes2 !== null){
+        botaoSelecionadoAntes2.classList.remove('bebidaselecionado')
+    }
+    
+        botaoClicado2.classList.add('bebidaselecionado')
+}
+
+function selecionarSobremesa(botaoClicado3) {
+    const botaoSelecionadoAntes3 = document.querySelector('.sobremesaselecionado')
+
+    if (botaoSelecionadoAntes3 !== null){
+        botaoSelecionadoAntes3.classList.remove('sobremesaselecionado')
+    }
+    
+        botaoClicado3.classList.add('sobremesaselecionado')
+}
